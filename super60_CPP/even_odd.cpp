fa2e191a213d90bca8f4316f6689030905e09960
Prateek Sharma <139517202+pkbros@users.noreply.github.com>
2024-12-05 14:32:21 +0530

    lo

---
+++ super60_CPP/even_odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter a number to check even or odd: ";
+    int a;
+    cin>>a;
+    if(a%2==0)
+    cout<<"Even"<<endl;
+    else
+    cout<<"Odd"<<endl;
+
+
+    return 0;
+}
