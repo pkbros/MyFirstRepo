d284dc6740fc9456fcfd08a094819af4f1cc039e
Prateek Sharma <139517202+pkbros@users.noreply.github.com>
2024-12-05 16:25:27 +0530

    .

---
+++ super60_CPP/classes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class signup
+{
+    string first_name;
+    string email;
+    string password;
+
+public:
+    void displayInfo()
+    {
+        cout << "Firstname" << first_name << endl;
+    }
+};
+
+int main()
+{
+    signup user;
+    user.first_name = "prateek";
+    user.
+}
