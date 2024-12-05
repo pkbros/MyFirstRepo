fb629e6a314f4bc89bcb3c56756f133692562ea5
Prateek Sharma <139517202+pkbros@users.noreply.github.com>
2024-12-05 15:40:26 +0530

    .

---
+++ super60_CPP/recursive_sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n){
+    if (n==0)
+    return 0;
+
+    else
+    return (n + sum(n-1));
+}
+
+int main(){
+
+    
+    cout<<(sum(4));
+
+    return 0;
+}
