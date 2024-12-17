de37400f976de262211ccfdb479343b3ed1ae499
Prateek Sharma <139517202+pkbros@users.noreply.github.com>
2024-12-17 13:50:33 +0530

    k

--- super60_CPP/class_work/len_str.cpp
+++ super60_CPP/class_work/len_str.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
-
-
+int main()
+{
+    char name[10] = "Prateek";
+    int count=0;
+    for (int i = 0; name[i] != '\0'; i++)
+    {
+        count = count + 1;
+    }
+    cout<<"Length of '"<<name<< "' is "<< count<<endl;
 
     return 0;
 }
